312279dff950892b39272db7900f7b429c2f059e
ciach0_ <ciach795@gmail.com>
2025-07-14 14:23:18 +0200

    CDN: Return 404 if file does not exist

--- src/routes/cdn.ts
+++ src/routes/cdn.ts
@@ -1,7 +1,8 @@
 import {Elysia} from "elysia";
 
 export default new Elysia({ prefix: '/cdn' })
-    .get('/*', async ({ params: { '*': path } }) => {
+    .get('/*', async ({ status, params: { '*': path } }) => {
         const file = Bun.file(`${import.meta.dir}/../../../cdn/${path}`);
+        if (!await file.exists()) return status(404);
         return Buffer.from(await file.arrayBuffer());
     })
