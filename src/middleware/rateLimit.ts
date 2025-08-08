b21983e49792634056489057e633c3b410e8e1f1
ciach0_ <ciach795@gmail.com>
2025-08-08 18:14:11 +0200

    0.0.15B-1: Fix rate limits

--- src/middleware/rateLimit.ts
+++ src/middleware/rateLimit.ts
@@ -1,9 +1,8 @@
 export default async function rateLimit(ip: string, limit: number = 100, windowMs: number, key: string) {
     const _key = `rate-limit:${ip}:${key}`;
-
     const count = await Bun.redis.incr(_key);
-    if (count === 1) await Bun.redis.expire(_key, windowMs);
 
+    if (count === 1) await Bun.redis.expire(_key, windowMs / 1000);
     const ttl = await Bun.redis.ttl(_key);
 
     return {
